e5bf5ede946812cfe0adda1e788d17aaf55b8dde
root <1@>
2024-11-21 12:38:33 +0000

    add install python3

--- scripts/deps/install_deps_anolisos.sh
+++ scripts/deps/install_deps_anolisos.sh
@@ -1,5 +1,5 @@
 yum update
 yum groupinstall "Development Tools" -y
 yum install -y epel-release
-yum install -y sudo git ninja-build cmake gdb perf libstdc++-static
+yum install -y sudo git ninja-build cmake gdb perf libstdc++-static python39
 sh -c 'echo -1 > /proc/sys/kernel/perf_event_paranoid'
