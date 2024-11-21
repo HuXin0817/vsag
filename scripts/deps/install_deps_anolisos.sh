a5237721ab3a0186c69657ba6188b2a7d6bd1bf0
HuXin0817 <202219120810@stu.cdut.edu.cn>
2024-11-21 13:54:40 +0800

    add devcontainer

--- scripts/deps/install_deps_anolisos.sh
+++ scripts/deps/install_deps_anolisos.sh
@@ -1,5 +1,5 @@
 yum update
 yum groupinstall "Development Tools" -y
 yum install -y epel-release
-yum install -y sudo git ninja-build cmake gdb perf libstdc++-static ccache
+yum install -y sudo git ninja-build cmake gdb perf libstdc++-static
 sh -c 'echo -1 > /proc/sys/kernel/perf_event_paranoid'
